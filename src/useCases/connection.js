c2f334b1cf2ff15553063d552e22c5944e94c793
Santiago Sanchez Taborda <santiago.sanchez.t@gmail.com>
2018-09-12 22:26:43 -0500

    refactor(Connection): Configure logger

--- src/useCases/connection.js
+++ src/useCases/connection.js
@@ -21,21 +21,21 @@ const db = Mongoose.createConnection(config.mongodb.uri, {
 
 // Add event listeners for mongo connection
 db.on('error', (err) => {
-  logger.error(`(pui-mongoconnection-module): connection error event: ${err.message}`);
+  logger.error(`(rsp-mongoconnection-module): connection error event: ${err.message}`);
   process.exit(1);
 });
 
-db.once('open', () => logger.info('(pui-mongoconnection-module): connection opened with DB'));
+db.once('open', () => logger.info('(rsp-mongoconnection-module): connection opened with DB'));
 
-db.on('connected', () => logger.info(`(pui-mongoconnection-module): Mongoose connection is open to: ${config.mongodb.uri}`));
+db.on('connected', () => logger.info(`(rsp-mongoconnection-module): Mongoose connection is open to: ${config.mongodb.uri}`));
 
-db.on('disconnected', () => logger.info('(pui-mongoconnection-module): Mongoose connection is disconnected'));
+db.on('disconnected', () => logger.info('(rsp-mongoconnection-module): Mongoose connection is disconnected'));
 
 
 // Proccess events signals
 process.on('SIGINT', () => {
   db.close(() => {
-    logger.info('(pui-mongoconnection-module): Mongoose connection is disconnected due to application termination');
+    logger.info('(rsp-mongoconnection-module): Mongoose connection is disconnected due to application termination');
     process.exit(1);
   });
 });
